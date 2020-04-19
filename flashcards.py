96facdcd5ea176654b8c4e7e8e93d4bcdfeb9cbd
Diogo <akio.miyake@live.com>
2020-04-19 15:40:16 -0300

    Module6 Finished

--- flashcards.py
+++ flashcards.py
@@ -25,36 +25,39 @@ def card_view(index):
     except IndexError:
         abort(404)
 
-@app.route("/add_card", methods=["GET", "POST"])
+
+@app.route('/add_card', methods=["GET", "POST"])
 def add_card():
-    if request.method=="POST":
+    if request.method == "POST":
+        # form has been submitted, process data
         card = {"question": request.form['question'],
                 "answer": request.form['answer']}
         db.append(card)
         save_db()
         return redirect(url_for('card_view', index=len(db)-1))
-
     else:
         return render_template("add_card.html")
 
-@app.route("/remove_card/<int:index>", methods=["GET", "POST"])
+
+@app.route('/remove_card/<int:index>', methods=["GET", "POST"])
 def remove_card(index):
     try:
         if request.method == "POST":
             del db[index]
             save_db()
-            return redirect(url_for("welcome")
-
+            return redirect(url_for('welcome'))
         else:
             return render_template("remove_card.html", card=db[index])
-    except:
+    except IndexError:
         abort(404)
+
+
 @app.route("/api/card/")
 def api_card_list():
     return jsonify(db)
 
 
-@app.route('/api/card/<int:index>')
+@app.route("/api/card/<int:index>")
 def api_card_detail(index):
     try:
         return db[index]
